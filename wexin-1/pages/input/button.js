26a2df46e13219c49ac8970d4bfe64be1275581e
锐源 <1370164516@qq.com>
2023-10-11 19:42:42 +0800

    全局app()和页面page()

--- wexin-1/pages/input/button.js
+++ wexin-1/pages/input/button.js
@@ -8,7 +8,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    "username": "lzc",
+    "userinfo": {"age":"23"},
+    "sex":['男','女']
   },
   submit:function(e){
     console.log(e.detail.value)
@@ -16,37 +18,42 @@ Page({
 
   /**
    * 生命周期函数--监听页面加载
-   */
+   */ 
   onLoad(options) {
-
+    console.log("页面只会渲染一次load");
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-    console.log(app.globalData)
+    console.log("页面ready");
+    // console.log(app.globalData);
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    // this.setData({
+    //   "username": "刘德华"
+    // });
+    this.data.username = "黎明" //这个不会改视图的内容，上面会一起改
+    console.log("页面show");
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide() {
-
+    console.log("页面hide");
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
-
+    console.log("页面unload");
   },
 
   /**
@@ -67,6 +74,9 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage() {
-
+    return {
+      "title": "hello lzc"
+    }
+    console.log("button 分享")
   }
 })
